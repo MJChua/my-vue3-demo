83843ffe97554a6c5e41f151d8feb74c063a0aa1
CR_Michael.Chua 晁世杰 <michael.chua@criltds.com>
2024-05-05 23:59:29 +0800

    feature/#2 init several files and alter colors, css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <keep-alive>
+    <main class="home">
+      <TopHeader :title="TopHeaderTitle" />
+
+      <div class="middle-section">
+        <div class="middle-section__img" />
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </main>
+  </keep-alive>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TopHeader from '@/components/TopHeader'
+import HelloWorld from '@/components/HelloWorld'
 
 export default {
   name: 'HomeView',
   components: {
+    TopHeader,
     HelloWorld
+  },
+  setup (_) {
+    const TopHeaderTitle = {
+      text: 'Home',
+      routGo: '/'
+    }
+
+    return {
+      TopHeaderTitle
+    }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .home
+
+    .middle-section
+      margin 30px 0
+      padding 0 20px
+
+      &__img
+        width 100%
+        height 50vh
+        background url('~@/assets/image/home/doggy.jpg') center no-repeat
+        background-size cover
+        border-radius 20px
+        overflow hidden
+
+</style>
