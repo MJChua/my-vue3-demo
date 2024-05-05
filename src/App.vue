a49008860d41d463b7f38ab487a028a9bf7e3753
CR_Michael.Chua 晁世杰 <michael.chua@criltds.com>
2024-05-05 21:13:43 +0800

    init project #1

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,37 @@
   <router-view/>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+import { onMounted } from 'vue'
+
+export default {
+  name: 'App',
+  setup (_) {
+    const body = document.body
+    const computeStyle = window.getComputedStyle(body)
+    const defaultBgColor = computeStyle.backgroundColor
+
+    onMounted(() => {
+      console.warn('defaultBgColor:', defaultBgColor)
+      document.getElementById('app').style.background = defaultBgColor
+      // 比对域名
+      // const fuzzyCompare = () => {
+      //   const curUrl = location.host.split('.')
+      //   const compUrl = process.env.REPO_INFO.testDomain[process.env.VUE_APP_ENV_CONFIG].split('.')
+      //   const minLength = Math.min(compUrl.length, curUrl.length)
+
+      //   for (let i = 0; i < minLength; i++) {
+      //     return compUrl[i].includes(curUrl[i]) || curUrl[i].includes(compUrl[i])
+      //   }
+      //   return true
+      // }
+      // console.warn('innn:', fuzzyCompare())
+    })
+  }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true,
+    port: 9911
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        import: [
+          path.resolve(__dirname, 'src/assets/styles/theme.styl'),
+          path.resolve(__dirname, 'src/assets/styles/var.styl')
+        ]
+      }
+    }
+  }
 })
