cd1b5acdad4a0bf007c23331e6542f660a964f88
AzureAD\MichaelChao <michael.chao@odintechnology.com.tw>
2026-04-24 17:45:33 +0800

    chore(release): prepare release_0.2.0

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-﻿<template>
+<template>
   <router-view/>
   <GoToTop />
   <MobileBottomNav />
@@ -8,8 +8,7 @@
 import { onMounted, computed, getCurrentInstance } from 'vue'
 import { useAppStore } from './store/main'
 import { storeToRefs } from 'pinia'
-
-import { attachAutoResize } from '@/utils/resizeScreen'
+import { attachAutoResize, applyAppearanceClass } from '@my-vue3/core'
 import GoToTop from '@/components/GoToTop/index.vue'
 import MobileBottomNav from '@/components/MobileBottomNav/index.vue'
 
@@ -29,16 +28,10 @@ export default {
     })
 
     const detectAppearance = () => {
-      if (userAppearance.value === 'light') {
-        document.body.classList.add('light-mode')
-        document.body.classList.remove('dark-mode')
-      } else {
-        document.body.classList.add('dark-mode')
-        document.body.classList.remove('light-mode')
-      }
+      applyAppearanceClass(userAppearance.value)
     }
 
-    onMounted(async () => {
+    onMounted(() => {
       attachAutoResize(store.SetDevice, store.SetFontsize)
       detectAppearance()
     })
